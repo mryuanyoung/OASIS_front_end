fa5a89c2a206abcd54dc8908db7df6731900e8d4
yuanyoung <mryuanyoung@outlook.com>
2020-02-28 12:00:30 +0800

    add service worker

--- src/index.js
+++ src/index.js
@@ -15,4 +15,8 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if(navigator.serviceWorker){
+    navigator.serviceWorker.register('/sw.js')
+    .then(reg => console.log(reg))
+    .catch(err => console.log(err));
+}
